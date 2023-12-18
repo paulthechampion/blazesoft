df452233107a724433df0e0103505fa76e318da9
paulthechampion <enwerempaulo@gmail.com>
2023-12-18 10:32:27 -0500

    update hire me description

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,7 +9,7 @@ import malala from '../images/malala.webp'
 const initialBooks = [
   { id: 1, name: 'Learn React', price: '$20', category: 'Science', description: 'Description for Book 1', image: reactBook },
   { id: 2, name: 'The Alchemist', price: '$25', category: 'Fiction', description: 'Description for Book 2', image: alchemist },
-  { id: 3, name: 'Hire Me', price: '$130,000', category: 'Senior', description: 'Description for Book 3', image: hire },
+  { id: 3, name: 'Hire Me', price: '$130,000', category: 'Senior', description: 'I am a dedicated JavaScript Full-stack Software developer with over 5+ years of professional experience in MongoDB, Express JS, React JS, Redux JS, Node JS (M, E, R, R, N) and many more, seeking full-time employment as a Senior Software developer. ', image: hire },
   { id: 4, name: 'Richest Man', price: '$15', category: 'History', description: 'Description for Book 4', image: richest },
   { id: 5, name: 'Malala', price: '$18', category: 'True Story', description: 'Description for Book 5', image: malala },
 ];
